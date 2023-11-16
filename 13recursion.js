4e0272d8c4567af335472d82b652de8288afc8fb
DhakshaManiyan <121249364+DhakshaManiyan@users.noreply.github.com>
2023-11-17 03:49:03 +0530

    Create 13recursion.js

---
+++ 13recursion.js
@@ -0,0 +1,85 @@
+//RECURSION CHALLENGE SECTION SOLUTIONS
+/*Reverse Solution
+Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+function reverse(){
+  // add whatever parameters you deem necessary - good luck!
+}
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
+*/
+
+function reverse(str){
+	if(str.length <= 1) return str;
+	return reverse(str.slice(1)) + str[0];
+}
+
+/*isPalindrome
+Write a recursive function called isPalindrome which returns true 
+if the string passed to it is a palindrome (reads the same forward and backward). 
+Otherwise it returns false.
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+function isPalindrome(){
+  // add whatever parameters you deem necessary - good luck!
+}
+*/
+function isPalindrome(str){
+    if(str.length === 1) return true;
+    if(str.length === 2) return str[0] === str[1];
+    if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
+    return false;
+}
+
+/*someRecursive Solution
+Write a recursive function called someRecursive 
+which accepts an array and a callback. 
+The function returns true if a single value in the array returns true when passed to the callback. 
+Otherwise it returns false.
+// SAMPLE INPUT / OUTPUT
+// const isOdd = val => val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+function someRecursive(){
+  // add whatever parameters you deem necessary - good luck!
+}
+*/
+
+function someRecursive(array, callback) {
+    if (array.length === 0) return false;
+    if (callback(array[0])) return true;
+    return someRecursive(array.slice(1),callback);
+}
+
+/*
+flatten Solution
+Write a recursive function called flatten which accepts an array of arrays 
+and returns a new array with all values flattened.
+function flatten(){
+  // add whatever parameters you deem necessary - good luck!
+}
+
+// flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+// flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+// flatten([[1],[2],[3]]) // [1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
+*/
+
+function flatten(oldArr){
+  var newArr = []
+  	for(var i = 0; i < oldArr.length; i++){
+    	if(Array.isArray(oldArr[i])){
+      		newArr = newArr.concat(flatten(oldArr[i]))
+    	} else {
+      		newArr.push(oldArr[i])
+    	}
+  } 
+  return newArr;
+}
